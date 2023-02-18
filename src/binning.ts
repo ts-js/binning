1a413330a0b5076621ae425d29724f41b3dbb658
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2023-02-18 18:29:07 +0300

    :feat: :data: initial commit

---
+++ src/binning.ts
@@ -0,0 +1,87 @@
+/**
+ * Structure of each bin
+ */
+export interface Bin {
+  /**
+   * Bin's label.
+   */
+  xAxis: {
+    value: number
+    label?: string
+  }
+
+  /**
+   * Values in a bin
+   */
+  values: number[]
+}
+
+/**
+ * Array type of bins
+ */
+export type Bins = Bin[]
+
+/**
+ * Convert input sample array of numbers into bins.
+ *
+ * @param sample Sample input
+ * @param binSize Number of bins
+ * @param labels Optional labels for the bins
+ */
+export function toBins(data: number[], binSize: number, labels?: string[]) {
+  const sampleCount = data.length
+
+  if (binSize >= sampleCount) {
+    throw new RangeError(
+      `Bin size ${binSize} should be less than sample size: ${sampleCount}`
+    )
+  }
+
+  // Sort data in descending order
+  const sortedData = data.sort((lhs, rhs) => lhs - rhs)
+  // Find min and max samples
+  const minSample = sortedData[0]
+  const maxSample = sortedData[sampleCount - 1]
+
+  // Create equal spaced bins
+  const binWidth = (maxSample - minSample) / binSize
+  const bins: Bins = Array(binSize)
+    .fill({ values: [] })
+    .map((bin, i) => ({
+      xAxis: {
+        value: minSample + binWidth * i,
+        label: labels?.[i]
+      },
+      ...bin
+    }))
+
+  // Use binary search to determine indices of minimum and maximum
+  // values for each bin
+  let minIndex = 0
+  let maxIndex = sampleCount - 1
+
+  for (let i = 0; i < binSize; i++) {
+    while (
+      minIndex < sampleCount &&
+      sortedData[minIndex] < bins[i].xAxis.value
+    ) {
+      minIndex++
+    }
+    while (
+      maxIndex >= 0 &&
+      sortedData[maxIndex] > bins[i].xAxis.value + binWidth
+    ) {
+      maxIndex--
+    }
+    for (let j = minIndex; j <= maxIndex; j++) {
+      bins[i].values.push(sortedData[j])
+    }
+
+    bins[i].values = sortedData.slice(minIndex, maxIndex + 1)
+
+    // Get the maxIndex ready for the next iteration.
+    maxIndex = sampleCount - 1
+  }
+
+  return bins
+}
---
+++ test/binning.test.ts
@@ -0,0 +1,54 @@
+import { toBins } from '../src/binning'
+
+describe('toBin', () => {
+  it('should create bins correctly with even bin size', () => {
+    const sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const binSize = 4
+    const bins = toBins(sample, binSize)
+
+    expect(bins.length).toBe(binSize)
+    expect(bins[0].xAxis.value).toBe(1)
+    expect(bins[1].xAxis.value).toBe(3.25)
+    expect(bins[2].xAxis.value).toBe(5.5)
+    expect(bins[3].xAxis.value).toBe(7.75)
+    expect(bins[0].values).toEqual(expect.arrayContaining([1, 2, 3]))
+    expect(bins[1].values).toEqual(expect.arrayContaining([4, 5]))
+    expect(bins[2].values).toEqual(expect.arrayContaining([6, 7]))
+    expect(bins[3].values).toEqual(expect.arrayContaining([8, 9, 10]))
+  })
+
+  it('should create bins correctly with odd bin size', () => {
+    const sample = [5, 10, 11, 13, 15, 35, 50, 55, 72, 92, 204, 215]
+    const binSize = 3
+    const bins = toBins(sample, binSize)
+
+    expect(bins.length).toBe(binSize)
+    expect(bins[0].xAxis.value).toBe(5)
+    expect(bins[1].xAxis.value).toBe(75)
+    expect(bins[2].xAxis.value).toBe(145)
+    expect(bins[0].values).toEqual(
+      expect.arrayContaining([5, 10, 11, 13, 15, 35, 50, 55, 72])
+    )
+    expect(bins[1].values).toEqual(expect.arrayContaining([92]))
+    expect(bins[2].values).toEqual(expect.arrayContaining([204, 215]))
+  })
+
+  it('should create bins with labels correctly', () => {
+    const sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const binSize = 4
+    const labels = ['a', 'b', 'c', 'd']
+    const bins = toBins(sample, binSize, labels)
+    expect(bins.length).toBe(binSize)
+    expect(bins[0].xAxis.label).toBe('a')
+    expect(bins[1].xAxis.label).toBe('b')
+    expect(bins[2].xAxis.label).toBe('c')
+    expect(bins[3].xAxis.label).toBe('d')
+  })
+
+  it('should throw an error if bin size is greater than or equal to sample length', () => {
+    const sample = [1, 2, 3]
+    const binSize = 3
+    expect(() => toBins(sample, binSize)).toThrow(RangeError)
+    expect(() => toBins(sample, 4)).toThrow(RangeError)
+  })
+})
